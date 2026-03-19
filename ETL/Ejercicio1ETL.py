258119fe4925dbca66321e8ee3999fea60f5f2d0
Jonatan Lerma <jonhylerma@hotmail.com>
2026-03-19 14:51:37 -0600

    Creo carpeta ETL y agrego archivos con ejercicios

---
+++ ETL/Ejercicio1ETL.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+#1. extract: leer el CSV
+
+df= pd.read_csv("orders.csv")
+print("Datos originales")
---
+++ ETL/Librerias.py
@@ -0,0 +1,20 @@
+
+# Crear un entorno virtual en VS Code para mantener tus dependencias ordenadas:
+# python -m venv venv                        -- aqui creamos un entorno virtual, dentro de este entorno es instalar las librerias de arriba
+# .\venv\Scripts\activate      # Windows     -- con esto activamos el entorno y podemos trabajar 
+
+
+# se deben instalar todas estas librerias para poder empezar a trabajar
+
+#- pandas → para manipulación y transformación de datos (nuestro núcleo de ETL)
+# pip install pandas
+
+#- openpyxl → para leer/escribir archivos Excel.
+# pip install openpyxl
+
+#- sqlalchemy → para conectarte a bases de datos SQL desde Python.
+# pip install pyodbc
+
+#- python-dotenv (opcional) → para manejar credenciales en un archivo .env.
+# pip install python-dotenv
+
