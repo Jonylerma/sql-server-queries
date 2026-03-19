d9223e2f2840bac7342fed6102bc524b744ad343
Jonatan Lerma <jonhylerma@hotmail.com>
2026-03-19 15:05:55 -0600

    Elimino archivos que no ocupo

--- ETL/01EjercicioETL.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy as sql
-import pandas as pd
-from sqlalchemy import create_engine
-
-#1. extract: leer el CSV
-
-df= pd.read_csv("orders.csv")
-print("Datos originales")
-print (df)
